7f6c43302bb17310f5fd1aa81d8912264e7d87a9
Tjaden Hess <tjade273@gmail.com>
2017-11-13 21:25:12 -0500

    Flesh out miner

--- miner.mli
+++ miner.mli
@@ -1,6 +1,13 @@
+(* The type of a mining instance *)
+type t
 
+(* [create pull_stream push_stream] is a new mining instance which can fetch
+ * new candidate block headers from [pull_stream] and push solved blocks back to [push_stream] *)
+val create : Block.header Lwt_stream.t -> (Block.header option -> unit) -> t
 
-(* [pow t b] is true if the SHA-256 hash of the input block [b] is less than or
- * equal to the target [t], and false otherwise. This indicates a sucessful 
- * proof of work. *)
-val pow : int -> Block.block -> bool
+(* [start miner cores] starts the mining worker [miner] running on
+ * [cores] CPU cores. *)
+val start : t -> int -> unit
+
+(* [stop miner] halts [miner]. [miner] can be restarted with [start] *)
+val stop : t -> unit
