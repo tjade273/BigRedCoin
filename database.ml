4a28ad38a1cd18396d05ff77bc9139bd8e9c19f0
Cosmo Viola <cjv48@cornell.edu>
2017-11-27 18:55:32 -0500

    Add mutex to database.ml

--- database.ml
+++ database.ml
@@ -6,8 +6,7 @@ module type Hashable = sig
   val serialize : t -> string
 
   val deserialize : string -> t
-end
-
+end 
 module type S = sig
 
   type t
@@ -18,45 +17,53 @@ module type S = sig
 
   val get : t -> string -> value Lwt.t
 
-  val get_opt : t -> string -> value option Lwt.t
-      
+  val get_opt : t -> string -> value option Lwt.t 
+
   val create : string -> t
 
-  val mem : t -> string -> bool Lwt.t
+  val mem : t -> string -> bool Lwt.t 
 
   val remove : t -> string -> unit Lwt.t
 
-end
+end 
+module Make(Value : Hashable) = struct
 
-module Make(Value : Hashable) : S = struct
-
-  type t = LevelDB.db
-
+  type t = LevelDB.db * Lwt_mutex.t ref
   type value = Value.t
 
-  let create dir = LevelDB.open_db dir
+  let create dir = LevelDB.open_db dir, ref (Lwt_mutex.create ())
 
-  let put db v = 
-    let f (db, v) = LevelDB.put db (Value.hash v) (Value.serialize v) in
-    Lwt_preemptive.detach f (db, v)
+  let put (db, mutex) v = 
+    Lwt_mutex.with_lock !mutex (fun () -> 
+      let f (db, v) = LevelDB.put db (Value.hash v) (Value.serialize v) in
+      Lwt_preemptive.detach f (db, v)
+    )
 
-  let get db v = 
-    let f (db, v) = Value.deserialize (LevelDB.get_exn db v) in
-    Lwt_preemptive.detach f (db, v)
+  let get (db, mutex) v = 
+    Lwt_mutex.with_lock !mutex (fun () ->
+      let f (db, v) = Value.deserialize (LevelDB.get_exn db v) in
+      Lwt_preemptive.detach f (db, v)
+    )
 
-  let get_opt db v = 
-    let f (db, v) =
-      match LevelDB.get db v with 
-      | None -> None
-      | Some s -> Some (Value.deserialize s) in
-    Lwt_preemptive.detach f (db, v)
+  let get_opt (db, mutex) v = 
+    Lwt_mutex.with_lock !mutex (fun () ->
+      let f (db, v) =
+        match LevelDB.get db v with 
+        | None -> None
+        | Some s -> Some (Value.deserialize s) in
+      Lwt_preemptive.detach f (db, v)
+    )
 
-  let mem db v = 
-    let f (db, v) = LevelDB.mem db v in
-    Lwt_preemptive.detach f (db, v)
+  let mem (db, mutex) v = 
+    Lwt_mutex.with_lock !mutex (fun () ->
+      let f (db, v) = LevelDB.mem db v in
+      Lwt_preemptive.detach f (db, v)
+    )
 
-  let remove db v =
-    let f (db, v) = LevelDB.delete db v in
-    Lwt_preemptive.detach f (db, v)
+  let remove (db, mutex) v =
+    Lwt_mutex.with_lock !mutex (fun () ->
+      let f (db, v) = LevelDB.delete db v in
+      Lwt_preemptive.detach f (db, v)
+    )
 
 end
